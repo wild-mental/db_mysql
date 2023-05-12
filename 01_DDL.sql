c294ffc3f6617894160c3d330a1ff8d0c02b359f
wild-mental <wildmental@knou.ac.kr>
2023-05-12 18:01:57 +0900

    SQL 2일차

---
+++ 01_DDL.sql
@@ -0,0 +1,99 @@
+/* 실행은 ctrl + enter
+데이터베이스 생성 명령
+데이터베이스 내부에 테이블들이 적재되기 때문에 먼저
+데이터베이스를 생성해야 합니다.
+DEFAULT CHARACTER SET UTF8; 을 붙여주시면 한글설정이 됩니다. 
+내가 지정하는 이름 등을 제외한 쿼리문은 대문자로만 작성하는게 일반적입니다.*/
+CREATE DATABASE bitcamp06 DEFAULT CHARACTER SET UTF8;
+
+/* 데이터베이스 조회는 좌측 하단 중간쯤의 Schemas를 클릭하고 -> 새로고침
+한 다음, bitcamp06이 생성된게 확인되면 우클릭 -> set as default schemas
+선택시 볼드처리되고, 지금부터 적는 쿼리문은 해당 DB에 들어간다는 의미임. */
+/* 해당 DB에 접근할 수 있는 사용자 계정 생성
+USER - id역할, IDENTIFIED BY - pw역할 */
+CREATE USER 'adminid' IDENTIFIED BY '2023502';
+
+/* 사용자에게 권한 부여 : GRANT 주고싶은기능1, 기능2,...
+만약 모든 권한을 주고 싶다면 ALL PRIVELEGES(모든권한) TO 부여받을계정명 */
+GRANT ALL PRIVILEGES ON bitcamp06.* to 'adminid';
+
+/* 테이블 생성 명령
+PRIMARY KEY : 컬럼의 주요 키를 뜻하고, 중복 데이터 방지도 겸함
+모든 테이블의 컬럼 중 하나는 반드시 PK속성이 부여되어 있어야 함.
+NOT NULL : 해당 컬럼을 비워둘 수 없다는 의미
+UNIQUE : 중복 데이터가 입력되는것을 방지함 */
+CREATE TABLE users(
+	u_number INT(3) PRIMARY KEY,
+	u_id VARCHAR(20) UNIQUE NOT NULL,
+    u_name VARCHAR(30) NOT NULL,
+    email VARCHAR(80)
+);
+
+/* 데이터 적재
+INSERT INTO 테이블명(컬럼1, 컬럼2...) VALUES(값1, 값2...);
+만약 모든 컬럼에 값을 넣는다면 위 구문에서 테이블명 다음 오는
+컬럼명을 생략할 수 있음.
+*/
+INSERT INTO users(u_number, u_id, u_name, email) VALUES 
+	(1, 'abc1234', '가나다', null);
+INSERT INTO users VALUES (2, 'abc3456', '마바사', 'abc@ab.com');
+
+/* 데이터 조회
+SELECT * FROM 테이블명; 을 적으면
+해당 테이블에 적재된 전체 컬럼의 데이터를 조회할 수 있습니다.
+SELECT (컬럼명1, 컬럼명2...) FROM 테이블명;
+을 이용해서 특정 컬럼에 적재된 데이터만 조회할 수도 있습니다.*/
+SELECT * FROM users;
+
+/* 문제
+3번 유저를 임의로 추가해주세요.
+조회구문은 이메일, 회원번호, 아이디 순으로 세 컬럼만 조회하는 구문*/
+INSERT INTO users(u_id, u_number, u_name) VALUE
+	('qwer1234', 3, '쿼리개발');
+SELECT email, u_number, u_id FROM users;
+
+/* 계정을 하나 더 생성하겠습니다.
+이번 계정은 SELECT 권한만 주겠습니다. */
+CREATE USER 'adminid2' IDENTIFIED BY '2023502';
+GRANT SELECT ON bitcamp06.* to 'adminid2';
+
+-- users 테이블에 주소 컬럼을 추가해보겠습니다 
+-- ALTER TABLE 테이블명 ADD ((추가컬럼명 자료타입(크기));
+ALTER TABLE users ADD (u_address varchar(30));
+
+SELECT * FROM users;
+
+-- users 테이블에서 이메일 컬럼을 삭제해주세요
+ALTER TABLE users DROP COLUMN email;
+
+-- u_address 컬럼에 UNIQUE 제약조건 별칭 부여해서 걸기
+ALTER TABLE users ADD CONSTRAINT 
+	u_address_unique UNIQUE (u_address);
+    
+-- INSERT INTO 구문으로 7, 8번 유저를 추가해주시되
+-- 주소는 둘 다 '강남구' 로 넣으려고 시도해보세요.
+INSERT INTO users VALUES(7, 'sevenseven', '칠칠이', '강남구');
+INSERT INTO users VALUES(8, 'eeeeeight', '팔팔이', '강남구');
+
+SELECT * FROM users;
+
+-- 주소에 걸린 unique제약을 없애고 8번을 마저 추가해주세요.
+-- 위에서 u_address컬럼에 걸린 제약조건의 별칭인 u_address_unique를 삭제
+ALTER TABLE users DROP CONSTRAINT u_address_unique;
+
+
+-- users테이블명을 members로 바꾸겠습니다.
+RENAME TABLE users to members;
+SELECT * FROM users;
+
+-- TRUNCATE TABLE은 내부 데이터만 소각합니다. 빈테이블은 남아있습니다.
+TRUNCATE TABLE members;
+SELECT * FROM members;
+
+-- DROP TABLE은 내부 데이터 및 테이블 구조 자체를 없앱니다.
+DROP TABLE members;
+SELECT * FROM members;
+
+
+
+
---
+++ 02_DML.sql
@@ -0,0 +1,144 @@
+/* 좌측의 SCHEMAS의 DATABASE는 더블클릭이나 우클릭으로도 호출할수있지만
+use DB명; 을 수행해 호출할수도 있습니다.*/
+
+-- Workbench(윈도우)에서 수행가능한 구문은 거의 모두 CLI환경에서 수행 가능합니다
+-- DATABASE 변경 구문;
+use sys; -- use bitcamp06;
+
+/* DATABASE 정보 조회 */
+show DATABASES;
+
+-- 테이블생성
+CREATE TABLE user_tbl (
+	user_num int(5) PRIMARY KEY AUTO_INCREMENT, -- INSERT시 숫자 자동배정
+    user_name varchar(10) NOT NULL,
+    user_birth_year int NOT NULL,
+    user_address char(5) NOT NULL,
+    user_height int, -- 자리수 제한 없음
+    entry_date date -- 회원가입일
+);
+
+/* 특정 테이블은 원래 조회할 때 
+SELECT * FROM 데이터베이스명.테이블명; 
+형식으로 조회해야 합니다.
+그러나 use 구문 등을 이용해 데이터베이스를 지정한 경우는 데이터베이스를 생략할 수 있습니다.*/
+SELECT * FROM bitcamp06.user_tbl;
+use bitcamp06;
+			-- pk가 걸린 컬럼은 null을 주면 알아서 숫자 배정
+INSERT INTO user_tbl VALUES(null, '김자바', 1987, '서울', 180, '2020-05-03');
+INSERT INTO user_tbl VALUES(null, '이연희', 1992, '경기', 164, '2020-05-12');
+INSERT INTO user_tbl VALUES(null, '박종현', 1990, '부산', 177, '2020-06-01');
+INSERT INTO user_tbl
+	(user_name, user_birth_year, user_address, user_height, entry_date)
+	VALUES ('신영웅', 1995, '광주', 172, '2020-07-15');
+
+-- WHERE 조건절을 이용해서 조회해보겠습니다.
+-- 90년대 이후 출생자만 조회하기      user_birth_year가 1989보다 큰 유저만
+SELECT * FROM user_tbl WHERE user_birth_year > 1989;
+-- 키 175 미만만 조회하는 SELECT 구문을 만들어서 보내주세요.
+SELECT * FROM user_tbl WHERE user_height < 175;
+-- AND 혹은 OR을 이용해 조건을 두 개 이상 걸수도 있습니다.
+SELECT * FROM user_tbl WHERE user_num > 2 AND user_height < 178; 
+
+-- UPDATE FROM 테이블명 set 컬럼명1=대입값1, 컬럼명2=대입값2....;
+-- 주의! WHERE를 걸지 않으면 해당 컬럼의 모든 값을 다 통일시켜버립니다.
+UPDATE user_tbl SET user_address = '서울';
+
+-- WHERE절 없는 UPDATE 구문 실행 방지, 0대입시 해제, 1대입시 실행
+set sql_safe_updates=1;
+
+-- 테이블이 존재하지 않다면 삭제구문을 실행하지 않아 에러를 발생시키지 않음
+DROP TABLE IF EXISTS user_tbl;
+
+
+SELECT * FROM user_tbl;
+
+-- 1번유저 김자바가 이사를 강원으로 갔습니다. 지역을 서울에서 강원으로 바꿔보겠습니다.
+UPDATE user_tbl SET user_address = '강원' WHERE user_num = 1;
+
+-- 삭제는 특정컬럼만 떼서 삭제할일이 없으므로 SELECT와는 달리 * 등을 쓰지 않습니다.
+-- 박종현이 DB에서 삭제되는 상황, safety 모드를 우회하거나 cli에서 해주세요.
+DELETE FROM user_tbl WHERE user_name = '박종현';
+
+-- 만약 WHERE없이 삭제시 truncate와 비슷하게 작동합니다.
+DELETE FROM user_tbl;
+
+-- 다중 INSERT구문을 사용해보겠습니다.
+/* INSERT INTO 테이블명 (컬럼1, 컬럼2, 컬럼3...)
+	VALUES (값1, 값2, 값3 ...),
+			(값4, 값5, 값6 ...),
+            (값7, 값8, 값9 ...),
+            ...;
+	컬럼명은 모든 컬럼에 값을 집어넣을시 생략 가능합니다.*/
+INSERT INTO user_tbl
+	VALUES (null, '강개발', 1994, '경남', 178, '2020-08-02'),
+			(null, '최지선', 1998, '전북', 170, '2020-08-03'),
+            (null, '류가연', 2000, '전남', 158, '2020-08-20');
+SELECT * FROM user_tbl;		
+            
+
+-- INSERT~SELECT 를 이용한 데이터 삽입을 위해 user_tbl과 동일한 테이블을 하나 더 만듭니다.
+CREATE TABLE user_tbl2 (
+	user_num int(5) PRIMARY KEY AUTO_INCREMENT, -- INSERT시 숫자 자동배정
+    user_name varchar(10) NOT NULL,
+    user_birth_year int NOT NULL,
+    user_address char(5) NOT NULL,
+    user_height int, -- 자리수 제한 없음
+    entry_date date -- 회원가입일
+);
+
+-- user_tbl2에 user_tbl의 자료 중 생년 1995년 이후인 사람의 자료만 복사해서 삽입하기
+INSERT INTO user_tbl2 
+	SELECT * FROM user_tbl
+    WHERE user_birth_year > 1995;
+
+-- 두 번째 테이블인 구매내역을 나타내는 buy_tbl을 생성해보겠습니다.
+-- 어떤 유저가 무엇을 샀는지 저장하는 테이블입니다.
+-- 어떤 유저는 반드시 user_tbl에 존재하는 유저만 추가할 수 있습니다.
+CREATE TABLE buy_tbl (
+	buy_num INT AUTO_INCREMENT PRIMARY KEY,
+	user_num INT(5) NOT NULL,
+    prod_name varchar(10) NOT NULL,
+    prod_cate varchar(10) NOT NULL,
+    price INT NOT NULL,
+    amount INT NOT NULL
+);
+
+-- 외래키 설정없이 추가해보겠습니다.
+INSERT INTO buy_tbl VALUES(null, 4, '아이패드', '전자기기', 100, 1);
+INSERT INTO buy_tbl VALUES(null, 4, '애플펜슬', '전자기기', 15, 1);
+INSERT INTO buy_tbl VALUES
+	(null, 6, '트레이닝복', '의류', 10, 2),
+    (null, 5, '안마의자', '의료기기', 400, 1),
+    (null, 2, 'SQL책', '도서', 2, 1);
+
+-- 있지도 않은 99번 유저의 구매내역을 넣어보겠습니다.
+INSERT INTO buy_tbl values(null, 99, '핵미사일', '전략무기', 100000, 5);
+
+
+DELETE FROM buy_tbl WHERE buy_num = 6;
+
+-- 이제 외래키 설정을 통해서, 있지 않은 유저는 등록될 수 없도록 처리하겠습니다.
+-- buy_tbl이 user_tbl을 참조하는 관계임.
+		-- 참조하는                                   -- 참조자의 어떤 컬럼
+ALTER TABLE buy_tbl ADD CONSTRAINT FK_buy_tbl FOREIGN KEY (user_num)
+		-- 참조당하는 테이블과 컬럼명
+	REFERENCES user_tbl(user_num);
+
+-- 참조가 끝난 상태에서 있지도 않은 99번 유저의 구매내역을 넣어보겠습니다.
+INSERT INTO buy_tbl values(null, 199, '오픈카', '승용차', 1000, 5);
+
+SELECT * FROM buy_tbl;
+SELECT * FROM user_tbl;
+-- 만약 user_tbl에 있는 요소를 삭제시, buy_tbl에 구매내역이 남은 user_num을 삭제한다면
+-- 특별히 on_delete를 걸지 않은 경우는 참조 무결성 원칙에 위배되어 삭제가 되지 않습니다.
+DELETE FROM user_tbl WHERE user_num=4;
+
+-- 만약 추가적인 설정 없이 user_tbl의 4번유저를 삭제하고 싶다면
+-- 먼저 buy_tbl의 4번유저가 남긴 구매내역을 모두 삭제해야 합니다.
+DELETE FROM buy_tbl WHERE buy_num = 1;
+DELETE FROM buy_tbl WHERE buy_num = 2;
+
+
+
+
